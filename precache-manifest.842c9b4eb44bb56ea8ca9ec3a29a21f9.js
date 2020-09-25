8e9e22d870c2d59c264ff701b52a477b9fd56091
Banovacki Ivan <banovacki.ivan@gmail.com>
2020-09-25 11:05:59 +0200

    Updates

---
+++ precache-manifest.842c9b4eb44bb56ea8ca9ec3a29a21f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d244d02897308bf470316f852408a3e4",
+    "url": "/react-esports-app/index.html"
+  },
+  {
+    "revision": "f866909b3426498038ca",
+    "url": "/react-esports-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "abc26a9b6cba11409bac",
+    "url": "/react-esports-app/static/js/2.f2d17db0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-esports-app/static/js/2.f2d17db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f866909b3426498038ca",
+    "url": "/react-esports-app/static/js/main.92ef8aff.chunk.js"
+  },
+  {
+    "revision": "cf6a8d715117f96d4920",
+    "url": "/react-esports-app/static/js/runtime-main.48d6f5b1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/%7Brepo-name%7D/precache-manifest.a16acfa569efc48bd645a31b2b5d99be.js"
+  "/react-esports-app/precache-manifest.842c9b4eb44bb56ea8ca9ec3a29a21f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7Brepo-name%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-esports-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
