a3f838272bd753768abdc8e5f37e8ff41a414664
Banovacki Ivan <banovacki.ivan@gmail.com>
2020-09-25 11:04:36 +0200

    Updates

---
+++ precache-manifest.a16acfa569efc48bd645a31b2b5d99be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9129eb5144fd7daa04ab8bc89fd632c",
+    "url": "/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "15b7e9efc3f27715b4ab",
+    "url": "/%7Brepo-name%7D/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "418c76cb2b9e0d9e2a5f",
+    "url": "/%7Brepo-name%7D/static/js/2.67b51c55.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/%7Brepo-name%7D/static/js/2.67b51c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b7e9efc3f27715b4ab",
+    "url": "/%7Brepo-name%7D/static/js/main.9fd5e8e4.chunk.js"
+  },
+  {
+    "revision": "c5f1b2e274e891387276",
+    "url": "/%7Brepo-name%7D/static/js/runtime-main.3b16bc12.js"
+  }
+]);
