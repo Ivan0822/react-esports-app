ec3906b347d8ec774cef8380bf9cffc15a77bc18
Banovacki Ivan <banovacki.ivan@gmail.com>
2020-09-29 22:10:00 +0200

    Updates

--- precache-manifest.37b2e4e2d83198e9f5e066002e72006f.js
+++ precache-manifest.37b2e4e2d83198e9f5e066002e72006f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d97715190552891850e24811c573687",
+    "revision": "466ccb8daea4ee46a7952dcb071aa87d",
     "url": "/react-esports-app/index.html"
   },
   {
-    "revision": "657672516f283d4e23d2",
+    "revision": "9c3c9333316f537024db",
     "url": "/react-esports-app/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "e98c1ecea87090b8d2fe",
-    "url": "/react-esports-app/static/js/2.d50ec454.chunk.js"
+    "revision": "3fe7501724b6aa38fb19",
+    "url": "/react-esports-app/static/js/2.940f07e9.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/react-esports-app/static/js/2.d50ec454.chunk.js.LICENSE.txt"
+    "url": "/react-esports-app/static/js/2.940f07e9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "657672516f283d4e23d2",
-    "url": "/react-esports-app/static/js/main.60d94fef.chunk.js"
+    "revision": "9c3c9333316f537024db",
+    "url": "/react-esports-app/static/js/main.2acf1153.chunk.js"
   },
   {
     "revision": "cf6a8d715117f96d4920",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-esports-app/precache-manifest.53cc5d44ac81b405e5dcce0bc884c389.js"
+  "/react-esports-app/precache-manifest.37b2e4e2d83198e9f5e066002e72006f.js"
 );
 
 self.addEventListener('message', (event) => {
